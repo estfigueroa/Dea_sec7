805e7747f76deb65c1e8042cf4a0ec0c845c0c75
AO-Alumno <AO-Alumno@duoc.acad>
2026-06-24 10:38:52 -0400

    Listo los validadores y el imprimir

---
+++ modulo_usuarios.py
@@ -0,0 +1,38 @@
+usuarios_list = []
+
+#--------- validaciones -------------
+def validar_sexo(sexo):
+    if sexo in ["M", "F"]:
+        return True
+    else:
+        print("ERROR es valido F o M")
+        return False
+
+def validar_password(password):
+    if len (password.strip())>8:
+        print("ERROR, minimo 8 caracteres")
+        return False
+    
+    tiene_numero = False
+    for letra in password:
+        if letra.isnumeric():
+            tiene_numero = True
+    
+    tiene_letra = False
+    for letra in password:
+        if letra.isalpha():
+            tiene_letra = True
+
+    if " " in password:
+        print("ERROR, no puede estar vacio")
+        return False
+
+def imprimir_usuario(usuario):
+    print(f"""
+        ---------------------------------
+        Nombre usuario: {usuario["nombre_usuario"]}
+        Sexo: {usuario["sexo"]}
+        Password: {usuario["password"]}
+        """)
+
+#---------- transacciones --------------
